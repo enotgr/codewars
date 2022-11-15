e0fafcb948d80e387df8c2380396423914a7b527
Mikhail Krotov <genry.foer@gmail.com>
2022-11-15 13:12:37 +0600

    kata 13

---
+++ js/codewars-013.js
@@ -0,0 +1,15 @@
+// Counting Duplicates | 6kyu
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+function duplicateCount(text) {
+  const res = text.toLowerCase().split('').reduce((acc, x) => {
+    if (Object.keys(acc).includes(x)) {
+      acc[x] += 1;
+    } else {
+      acc[x] = 0;
+    }
+    return acc;
+  }, {})
+  return Object.keys(res).filter(key => res[key]).length;
+}
+
+// console.log(duplicateCount('aaBccdEee'));
